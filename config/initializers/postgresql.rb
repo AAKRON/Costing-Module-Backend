93e3260d7acb14b236cf32ee1d4d1356abfbfb22
nilu25 <nileshkumar.gupta4@gmail.com>
2021-07-20 00:09:43 +0530

    check postgressql issue

--- config/initializers/postgresql.rb
+++ config/initializers/postgresql.rb
@@ -1,1 +1,13 @@
+require "upsert"
+require "upsert/column_definition/postgresql"
+Upsert::ColumnDefinition::Postgresql.class_eval do
+	def all(connection, quoted_table_name)
+          res = connection.execute "SELECT a.attname AS name, format_type(a.atttypid, a.atttypmod) AS sql_type, pg_get_expr(d.adbin, d.adrelid) AS default
+	  FROM pg_attribute a LEFT JOIN pg_attrdef d
+	  ON a.attrelid = d.adrelid AND a.attnum = d.adnum
+	  WHERE a.attrelid = '#{quoted_table_name}'::regclass
+	  AND a.attnum > 0 AND NOT a.attisdropped"
+	end  
+end
+
 
