a9ae66ef69f22e99180630fa71bdadd0e6597076
Anthony Benavides Arce <abenavides@casa-andina.com>
2023-08-15 15:05:24 -0500

    Fix: Allow hosts in config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Allow hosts
+  config.hosts << /[a-z0-9]+\.railway\.app/
 end
