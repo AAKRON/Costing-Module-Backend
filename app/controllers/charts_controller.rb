09a0f3b6ecabc73c94f2ead53f934a5b54f27205
Anthony Benavides Arce <abenavides@casa-andina.com>
2023-10-18 10:05:10 -0500

    Feature: Add blanks count to charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,27 +3,55 @@ module Api
   module V1
     class ChartsController < BaseController
       before_action :restrict_access
-      before_action :set_jobs_count, only: [:get_charts_info]
+      before_action :set_jobs_charts, only: [:get_charts_info]
+      before_action :set_blanks_charts, only: [:get_charts_info]
 
       def get_charts_info
         result_data = {
             number_of_jobs_created_each_day: {
                 dates: @jobs_dates,
                 data: @jobs_counts
-            }
+            },
+            # number_of_jobs_created_each_month: {
+            #     dates: @jobs_months,
+            #     data: @jobs_months_counts
+            # },
+            number_of_blanks_created_each_day: {
+                dates: @blanks_dates,
+                data: @blanks_counts
+            },
+            # number_of_blanks_by_type: {
+            #     categories: @blanks_types,
+            #     data: @blanks_type_counts
+            # },
         }
         render json: result_data, status: :ok
       end
 
       private
 
-      def set_jobs_count
-        jobs_by_date = JobListing.order('DATE(created_at) DESC').group("DATE(created_at)").count
-
+      def set_jobs_charts
+        jobs_by_date = JobListing.order('DATE(created_at) ASC').group("DATE(created_at)").count
         @jobs_dates = jobs_by_date.keys
         @jobs_counts = jobs_by_date.values
+
+        # jobs_by_month = JobListing.order('MONTH(created_at) ASC').group("MONTH(created_at)").count
+        # @jobs_months = jobs_by_date.keys
+        # @jobs_months_counts = jobs_by_date.values
       end
 
+      def set_blanks_charts
+        blanks_by_date = Blank.order('DATE(created_at) ASC').group("DATE(created_at)").count
+        @blanks_dates = blanks_by_date.keys
+        @blanks_counts = blanks_by_date.values
+
+        # blanks_by_type = Blank.joins(:blank_type)
+        #                     .group('blank_types.name')
+        #                     .select('blank_types.name')
+        #                     .count
+        # @blanks_types = blanks_by_type.name
+        # @blanks_type_counts = blanks_by_type.count
+      end
     end
   end
 end
