eac4b41c1acdc96e197330da74960012313f8002
Anthony Benavides Arce <abenavides@casa-andina.com>
2023-10-24 22:54:47 -0500

    add blank numbers and blank types

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -12,18 +12,18 @@ module Api
                 dates: @jobs_dates,
                 data: @jobs_counts
             },
-            # number_of_jobs_created_each_month: {
-            #     dates: @jobs_months,
-            #     data: @jobs_months_counts
-            # },
+            number_of_jobs_created_each_month: {
+                dates: @jobs_months,
+                data: @jobs_months_counts
+            },
             number_of_blanks_created_each_day: {
                 dates: @blanks_dates,
                 data: @blanks_counts
             },
-            # number_of_blanks_by_type: {
-            #     categories: @blanks_types,
-            #     data: @blanks_type_counts
-            # },
+            number_of_blanks_by_type: {
+                categories: @blanks_types,
+                data: @blanks_type_counts
+            },
         }
         render json: result_data, status: :ok
       end
@@ -31,33 +31,47 @@ module Api
       private
 
       def set_jobs_charts
-        jobs_by_date = JobListing.where("created_at > ?", "%#{params[:start_date]}%")
-        .where("created_at < ?", "%#{params[:end_date]}%")
-        .order('DATE(created_at) ASC')
-        .group("DATE(created_at)").count
+        # Get jobs with the filters applied
+        jobs_with_filters = JobListing.where("job_listings.created_at > ?", "%#{params[:start_date]}%")
+                                        .where("job_listings.created_at < ?", "%#{params[:end_date]}%")
+
+        # Get jobs by date
+        jobs_by_date = jobs_with_filters
+                        .order('DATE(created_at) ASC')
+                        .group("DATE(created_at)").count
+
         @jobs_dates = jobs_by_date.keys
         @jobs_counts = jobs_by_date.values
 
-        # jobs_by_month = JobListing.order('MONTH(created_at) ASC').group("MONTH(created_at)").count
-        # @jobs_months = jobs_by_date.keys
-        # @jobs_months_counts = jobs_by_date.values
+        # Get jobs by month
+        jobs_by_month = jobs_with_filters
+        .group('extract(month from created_at)').count
+
+        @jobs_months = jobs_by_date.keys
+        @jobs_months_counts = jobs_by_date.values
       end
 
       def set_blanks_charts
-        blanks_by_date = Blank.where("created_at > ?", "%#{params[:start_date]}%")
-        .where("created_at < ?", "%#{params[:end_date]}%")
-        .order('DATE(created_at) ASC')
-        .group("DATE(created_at)").count
+        # Get blanks with the filters applied
+        blanks_with_filters = Blank.where("blanks.created_at > ?", "%#{params[:start_date]}%")
+                                .where("blanks.created_at < ?", "%#{params[:end_date]}%")
+
+        # Get blanks by date
+        blanks_by_date = blanks_with_filters
+                            .order('DATE(created_at) ASC')
+                            .group("DATE(created_at)").count
 
         @blanks_dates = blanks_by_date.keys
         @blanks_counts = blanks_by_date.values
 
-        # blanks_by_type = Blank.joins(:blank_type)
-        #                     .group('blank_types.name')
-        #                     .select('blank_types.name')
-        #                     .count
-        # @blanks_types = blanks_by_type.name
-        # @blanks_type_counts = blanks_by_type.count
+        # Get blanks per type
+        blanks_by_type = blanks_with_filters
+                            .left_outer_joins(:blank_type)
+                            .group('blank_types.description')
+                            .count
+
+        @blanks_types = blanks_by_type.keys
+        @blanks_type_counts = blanks_by_type.values
       end
     end
   end
