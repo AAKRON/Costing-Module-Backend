5d2a4be4c931997c2916e6afb85845ea9ae21edc
DanzsvTB <rogelio.sandoval@threebranches.io>
2023-10-31 17:58:23 -0500

    PDF fixed

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,8 +1,14 @@
 # config/initializers/pdfkit.rb
+
+#HEROKU
+# PDFKit.configure do |config|
+#   config.wkhtmltopdf = Gem.bin_path("wkhtmltopdf-heroku", "wkhtmltopdf-linux-amd64")
+#   config.default_options = {
+#     :page_size => 'Legal',
+#     :print_media_type => true
+#   }
+# end
+
 PDFKit.configure do |config|
-  config.wkhtmltopdf = Gem.bin_path("wkhtmltopdf-heroku", "wkhtmltopdf-linux-amd64")
-  config.default_options = {
-    :page_size => 'Legal',
-    :print_media_type => true
-  }
+  config.wkhtmltopdf = `which wkhtmltopdf`.to_s.strip
 end
