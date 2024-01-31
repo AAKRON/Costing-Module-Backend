4ccefa202d0087d71a333a900f82c922c872ccd1
Anthony Benavides Arce <abenavides@casa-andina.com>
2024-01-31 11:21:17 -0500

    Fix: update locations

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -10,7 +10,7 @@ module Api
 
         def index
           id = (params.fetch(:id, '') == 'null' ) ? '' : params.fetch(:id, '')
-          @locations = Location.order('id ASC').where('active_flag = 1').paginate(params.slice(:_end, :_sort, :_order))
+          @locations = Location.order('active_flag DESC').paginate(params.slice(:_end, :_sort, :_order))
           @locations = @locations.search(id, :id) unless id.empty?
           @locations = @locations.search(params[:name], :name) unless params.fetch(:name, '').empty?
 
@@ -46,7 +46,7 @@ module Api
         end
 
         def location_list_only
-          @locations = Location.where("active_flag = 1")
+          @locations = Location.where("active_flag = 1").order('id ASC')
 
           render json: @locations, status: :ok
         end
