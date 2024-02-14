8c9e47d4081ef36931e0ca78132ddfda22ea6c41
Anthony Benavides Arce <abenavides@casa-andina.com>
2024-02-13 23:41:05 -0500

    Feature: Update item location prices

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,33 +9,24 @@ module Api
       after_action(only: [:item_list_only]) { set_pagination_header(Item.count) }
 
       def index
-        #set_pagination_header(ItemCostView.count)
-        item_id = (params.fetch(:item_id, '') == 'null' ) ? '' : params.fetch(:item_id, '')
+        item_number = (params.fetch(:item_id, '') == 'null' ) ? '' : params.fetch(:item_id, '')
+        description = params.fetch(:description, '')
+        type_description = params.fetch(:type_description, '')
+        box_name = params.fetch(:box_name, '')
+        number_of_pcs_per_box = params.fetch(:number_of_pcs_per_box, '')
         ink_cost = (params.fetch(:ink_cost, '') == 'null' ) ? '' : params.fetch(:ink_cost, '')
         box_cost = (params.fetch(:box_cost, '') == 'null' ) ? '' : params.fetch(:box_cost, '')
         secondary_box_cost = (params.fetch(:secondary_box_cost, '') == 'null' ) ? '' : params.fetch(:secondary_box_cost, '')
         total_price_cost = (params.fetch(:total_price_cost, '') == 'null' ) ? '' : params.fetch(:total_price_cost, '')
         total_inventory_cost = (params.fetch(:total_inventory_cost, '') == 'null' ) ? '' : params.fetch(:total_inventory_cost, '')
+        @secondary_box_id_exists = ActiveRecord::Base.connection.column_exists?(:items, :secondary_box_id)
 
         _start = params[:_start].to_i
-        _end = params[:_end].to_i
-        # @items = ItemCostView.paginate(params.slice(:_end, :_sort, :_order))
-        @items = ItemCostView.order("#{params[:_sort]} #{params[:_order]}").offset(_start).limit(_end - _start)
-        @items = @items.search(item_id, :item_number) unless item_id.empty?
-        @items = @items.search(params[:description], :description) unless params.fetch(:description, '').empty?
-        @items = @items.search(params[:type_description], :type_description) unless params.fetch(:type_description, '').empty?
-        @items = @items.where("lower(box_name) LIKE ?", "%#{params[:box_name]}%") unless params.fetch(:box_name, '').empty?
-        @items = @items.where("number_of_pcs_per_box = #{params[:number_of_pcs_per_box]}") unless params.fetch(:number_of_pcs_per_box, '').empty?
-        @items = @items.search(ink_cost, :ink_cost) unless ink_cost.empty?
-        @items = @items.search(box_cost, :box_cost) unless box_cost.empty?
-        @items = @items.search(total_price_cost, :total_price_cost) unless total_price_cost.empty?
-        @items = @items.search(total_inventory_cost, :total_inventory_cost) unless total_inventory_cost.empty?
+        _limit = params[:_end].to_i - _start
+        _order = "#{params[:_sort]} #{params[:_order]}"
+        _location_id = @location ? @location.id : 0
 
-        @secondary_box_id_exists = ActiveRecord::Base.connection.column_exists?(:items, :secondary_box_id)
-        if @secondary_box_id_exists
-            @items = @items.search(secondary_box_cost, :secondary_box_cost) unless secondary_box_cost.empty?
-        end
-
+        @items = ItemCostView.filter_by_location(_location_id, _order, _start, _limit, item_number, description, type_description, box_name, number_of_pcs_per_box, ink_cost, box_cost, @secondary_box_id_exists, secondary_box_cost, total_price_cost, total_inventory_cost)
         render_items_template(template_name: :list, status: :ok)
       end
 
--- app/models/item_cost_view.rb
+++ app/models/item_cost_view.rb
@@ -2,6 +2,25 @@ class ItemCostView < ApplicationRecord
   include Paginatable
   include Searchable
 
+  scope :filter_by_location, ->(location_id, _order, _start, _limit, item_number, description, type_description, box_name, number_of_pcs_per_box, ink_cost, box_cost, secondary_box_id_exists, secondary_box_cost, total_price_cost, total_inventory_cost) {
+    where_clauses = "WHERE TRUE "
+    where_clauses = "#{where_clauses} AND item_number::text LIKE '#{item_number}%'" unless item_number.blank?
+    where_clauses = "#{where_clauses} AND LOWER(description) LIKE LOWER('%#{description}%')" unless description.blank?
+    where_clauses = "#{where_clauses} AND LOWER(type_description) LIKE LOWER('%#{type_description}%')" unless type_description.blank?
+    where_clauses = "#{where_clauses} AND LOWER(box_name) LIKE LOWER('#{box_name}%')" unless box_name.blank?
+    where_clauses = "#{where_clauses} AND number_of_pcs_per_box::text LIKE '#{number_of_pcs_per_box}%'" unless number_of_pcs_per_box.blank?
+    where_clauses = "#{where_clauses} AND ink_cost::text LIKE '#{ink_cost}%'" unless ink_cost.blank?
+    where_clauses = "#{where_clauses} AND box_cost::text LIKE '#{box_cost}%'" unless box_cost.blank?
+    where_clauses = "#{where_clauses} AND total_price_cost::text LIKE '#{total_price_cost}%'" unless total_price_cost.blank?
+    where_clauses = "#{where_clauses} AND total_inventory_cost::text LIKE '#{total_inventory_cost}%'" unless total_inventory_cost.blank?
+
+    if secondary_box_id_exists
+        where_clauses = "#{where_clauses} AND secondary_box_cost::text LIKE '#{secondary_box_cost}%'" unless secondary_box_cost.blank?
+    end
+
+    find_by_sql("SELECT * FROM get_item_costs(#{location_id}) #{where_clauses} ORDER BY #{_order} LIMIT #{_limit} OFFSET #{_start};")
+  }
+
   def self.to_price_csv
     CSV.generate(col_sep: ';') do |csv|
       csv << ["Item Number", "Description", "Item Type", "Cost For Price"]
