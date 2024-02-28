f16aa036c1fe0a3f7378bc1d3e12d4effaad10fe
Anthony Benavides Arce <abenavides@casa-andina.com>
2024-02-28 13:22:00 -0500

    Fix: Locations not changing between databases

--- app/controllers/api/v1/database_controller.rb
+++ app/controllers/api/v1/database_controller.rb
@@ -2,13 +2,6 @@ module Api
     module V1
         class DatabaseController < ApplicationController
             def list_databases
-                connection_config = Rails.application.config.database_configuration[Rails.env]
-                database = ENV['PG_DB_DEV']
-                if ActiveRecord::Base.connection.current_database != database
-                    connection_config['database'] = database
-                    ActiveRecord::Base.establish_connection(connection_config)
-                end
-
                 database_years = DatabaseYear.all
                 render json: database_years, status: :ok
             end
@@ -21,7 +14,7 @@ module Api
                     render json: { message: "Backup for #{current_year} already made" }, status: :bad_request
                 else
                     system("rake db:backup")
-                    # DatabaseYear.new(year: current_year).save
+                    DatabaseYear.new(year: current_year).save
                     render json: { message: "Backup completed." }
                 end
             end
