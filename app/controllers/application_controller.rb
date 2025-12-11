3b22c4544e4f3c603157b17dbb3cc516727c5f4d
Matt Kaufman <mkaufman@aakronline.com>
2025-12-11 12:25:44 -0500

    fix: Define restrict_access method in ApplicationController
    - Added restrict_access method to ApplicationController to prevent callback errors
    - Removed skip_before_action from SetupController that referenced non-existent callback
    - Fixes ArgumentError: Before process_action callback :restrict_access has not been defined
    - Allows controller hierarchy to function properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::API
   # protect_from_forgery with: :null_session
+  
+  # Define restrict_access method that can be optionally used by subcontrollers
+  private
+  
+  def restrict_access
+    # Default implementation - override in subcontrollers that need authentication
+    true
+  end
 end
