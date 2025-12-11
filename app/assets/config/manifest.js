20c8e796ebc6b28276f7cfad80a1f348b2af27c9
Matt Kaufman <mkaufman@aakronline.com>
2025-12-11 11:32:45 -0500

    fix: Add required asset pipeline structure for Sprockets
    - Created app/assets/config/manifest.js as required by sprockets-rails gem
    - Added basic asset directories (images, javascripts, stylesheets) with placeholders
    - Fixes Sprockets::Railtie::ManifestNeededError preventing application startup
    - Satisfies Rails asset pipeline requirements even in API-only mode

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+
+// This manifest file is required by Rails, even for API-only applications
+// when sprockets-rails gem is included. Since this is an API, we don't
+// need complex asset processing - this satisfies the requirement.
