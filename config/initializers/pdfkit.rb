35962c0daae620245872cdd616f3231e1e1aeda4
AAKRON <ITDEPARTMENT@AAKRONLINE.COM>
2026-07-02 17:23:00 -0400

    Fix pdfkit: use Gem.bin_path instead of which wkhtmltopdf
    `which wkhtmltopdf` is unreliable in Docker because PATH at init time
    may not include the gem bin dir. Gem.bin_path resolves the binary
    directly from the installed gem, regardless of PATH.

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,14 +1,9 @@
 # config/initializers/pdfkit.rb
 
-#HEROKU
-# PDFKit.configure do |config|
-#   config.wkhtmltopdf = Gem.bin_path("wkhtmltopdf-heroku", "wkhtmltopdf-linux-amd64")
-#   config.default_options = {
-#     :page_size => 'Legal',
-#     :print_media_type => true
-#   }
-# end
-
 PDFKit.configure do |config|
-  config.wkhtmltopdf = `which wkhtmltopdf`.to_s.strip
+  config.wkhtmltopdf = begin
+    Gem.bin_path("wkhtmltopdf-binary", "wkhtmltopdf")
+  rescue Gem::GemNotFoundException
+    `which wkhtmltopdf`.to_s.strip
+  end
 end
