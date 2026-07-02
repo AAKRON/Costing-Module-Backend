b55fa3372de282313d730b996ed63debef219607
AAKRON <ITDEPARTMENT@AAKRONLINE.COM>
2026-07-02 18:04:54 -0400

    Fix inspect_production: capture db name before closing connection

--- app/controllers/data_migration_controller.rb
+++ app/controllers/data_migration_controller.rb
@@ -9,6 +9,7 @@ class DataMigrationController < ApplicationController
 
     begin
       prod_conn = PG::Connection.new(prod_url)
+      db_name   = prod_conn.db
 
       tables = prod_conn.exec(
         "SELECT tablename FROM pg_tables WHERE schemaname = 'public' ORDER BY tablename"
@@ -24,7 +25,7 @@ class DataMigrationController < ApplicationController
 
       render json: {
         status: 'success',
-        production_database: prod_conn.db,
+        production_database: db_name,
         total_tables: tables.count,
         tables_with_counts: table_counts,
         timestamp: Time.current
