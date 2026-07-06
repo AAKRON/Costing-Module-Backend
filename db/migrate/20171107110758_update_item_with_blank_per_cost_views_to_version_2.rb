81bf1290787d9e5553f721b9f1818f0830ca7538
AAKRON <ITDEPARTMENT@AAKRONLINE.COM>
2026-07-06 09:38:06 -0400

    Fix migration class name mismatch: UpdateItemWithBlankPerCostViewsToVersion2

--- db/migrate/20171107110758_update_item_with_blank_per_cost_views_to_version_2.rb
+++ db/migrate/20171107110758_update_item_with_blank_per_cost_views_to_version_2.rb
@@ -1,4 +1,4 @@
-class UpdateItemWithBlankPerCostViewsToVersion2Original < ActiveRecord::Migration[5.0]
+class UpdateItemWithBlankPerCostViewsToVersion2 < ActiveRecord::Migration[5.0]
   def change
     drop_view :item_cost_views
     update_view :item_with_blank_per_cost_views, version: 2, revert_to_version: 1
